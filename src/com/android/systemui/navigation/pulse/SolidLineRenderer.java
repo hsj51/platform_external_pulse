de4f688326f6769efe526a9259eda283956841cc
ezio84 <brabus84@gmail.com>
2020-08-17 15:37:47 +0200

    Solid render: Code clean up and possibly improve performance
    - There's no real need to continuously draw an additional fade paint to solid bars, we can just set the custom user's alpha value once
    
    Change-Id: I4977e52c0e5b44fb17596dd699c88a2b69fcc863

--- src/com/android/systemui/navigation/pulse/SolidLineRenderer.java
+++ src/com/android/systemui/navigation/pulse/SolidLineRenderer.java
@@ -29,16 +29,17 @@ import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.PorterDuff.Mode;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 
+import androidx.core.graphics.ColorUtils;
+
 public class SolidLineRenderer extends Renderer {
     private Paint mPaint;
-    private Paint mFadePaint;
+    private int mUnitsOpacity = 255;
+    private int mColor = Color.WHITE;
     private ValueAnimator[] mValueAnimators;
     private FFTAverage[] mFFTAverage;
     private float[] mFFTPoints;
@@ -59,8 +60,6 @@ public class SolidLineRenderer extends Renderer {
         super(context, handler, view, colorController);
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        mFadePaint = new Paint();
-        mFadePaint.setXfermode(new PorterDuffXfermode(Mode.MULTIPLY));
         mDbFuzzFactor = 5f;
         mObserver = new CMRendererObserver(handler);
         mObserver.updateSettings();
@@ -182,7 +181,6 @@ public class SolidLineRenderer extends Renderer {
     @Override
     public void draw(Canvas canvas) {
         canvas.drawLines(mFFTPoints, mPaint);
-        canvas.drawPaint(mFadePaint);
     }
 
     @Override
@@ -200,7 +198,8 @@ public class SolidLineRenderer extends Renderer {
 
     @Override
     public void onUpdateColor(int color) {
-        mPaint.setColor(color);
+        mColor = color;
+        mPaint.setColor(ColorUtils.setAlphaComponent(mColor, mUnitsOpacity));
     }
 
     private class CMRendererObserver extends ContentObserver {
@@ -261,10 +260,11 @@ public class SolidLineRenderer extends Renderer {
                 mFFTAverage = null;
             }
 
-            int solidUnitsColor = Settings.System.getIntForUser(
+            mUnitsOpacity = Settings.System.getIntForUser(
                     resolver, Settings.System.PULSE_SOLID_UNITS_OPACITY, 200,
                     UserHandle.USER_CURRENT);
-            mFadePaint.setColor(Color.argb(solidUnitsColor, 255, 255, 255));
+
+            mPaint.setColor(ColorUtils.setAlphaComponent(mColor, mUnitsOpacity));
         }
 
         private void setupFFTAverage() {
